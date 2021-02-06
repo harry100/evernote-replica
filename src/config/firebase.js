7b76f99f2f047a81cc6b476b997d83aad9ca42bb
harry100 <maharjanamanda@gmail.com>
2021-02-06 19:55:34 +0545

    deployment setting

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,13 +2,16 @@ import { firebase } from "@firebase/app"
 
 require("firebase/firestore")
 
+const API_KEY = process.env.REACT_APP_API_KEY
+const APP_ID = process.env.REACT_APP_APP_ID
+
 const firebaseConfig = {
-  apiKey: "AIzaSyD_LYr2gwSUFt1jGQWI7KCVa2ncw1HxiJE",
+  apiKey: API_KEY,
   authDomain: "evernote-rep.firebaseapp.com",
   projectId: "evernote-rep",
   storageBucket: "evernote-rep.appspot.com",
   messagingSenderId: "320210903274",
-  appId: "1:320210903274:web:bae74e1b862a98933ef793",
+  appId: APP_ID ,
   measurementId: "G-X8V1V4HSM7",
 }
 
